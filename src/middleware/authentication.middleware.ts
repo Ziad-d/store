30efed67c91ef813ff202f5575953763898f18d0
Ziad-d <ziadmo62@gmail.com>
2022-03-15 13:12:53 +0200

    validation middleware

---
+++ src/middleware/authentication.middleware.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from 'express';
+import config from '../config';
+import jwt from 'jsonwebtoken';
+import Error from '../interfaces/error.interface';
+import routes from '../routes';
+
+const handleUnauthorizedError = (next: NextFunction) => {
+  const error: Error = new Error('Login Error, Please login again');
+  error.status = 401;
+  next(error);
+};
+
+const validateTokenMiddleware = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  try {
+    // get authHeader
+    const authHeader = req.get('Authorization');
+    // check authHeader validate
+    if (authHeader) {
+      // get value of token
+      const bearer = authHeader.split(' ')[0].toLowerCase();
+      const token = authHeader.split(' ')[1];
+      // check if it bearer token or not
+      if (token && bearer === 'bearer') {
+        // verify token -- decode based on tokenSecret
+        const decode = jwt.verify(
+          token,
+          config.tokenSecret as unknown as string
+        );
+        // next()
+        if (decode) {
+          next();
+          // failed to authenticate user
+        } else {
+          handleUnauthorizedError(next);
+        }
+        // token type not bearer
+      } else {
+        handleUnauthorizedError(next);
+      }
+      // no token provider
+    } else {
+      handleUnauthorizedError(next);
+    }
+  } catch (error) {
+    handleUnauthorizedError(next);
+  }
+};
+
+export default validateTokenMiddleware;
--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import * as controllers from '../../controllers/users.controllers';
+import authenticationMiddleware from '../../middleware/authentication.middleware';
 
 const routes = Router();
 
 // api/users
-routes.route('/').get(controllers.getMany).post(controllers.create);
+routes
+  .route('/')
+  .get(authenticationMiddleware, controllers.getMany)
+  .post(controllers.create);
 routes
   .route('/:id')
   .get(controllers.getOne)
@@ -13,4 +17,5 @@ routes
 
 // authentication
 routes.route('/authenticate').post(controllers.authenticate);
+
 export default routes;
